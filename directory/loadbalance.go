5e83733c8f019fd456793890d04433e6a8137b4d
zhapuyu <zhapuyu@sina.com>
2015-11-26 19:27:05 +0800

    directory starts

--- directory/loadbalance.go
+++ directory/loadbalance.go
@@ -5,8 +5,7 @@ type StoreList []*meta.Store
 
 // StoreState contains store state data
 type StoreState struct {
-	id              string
-	status          int           //RW permissions
+	Id              string
 	restSpace       int           //rest space of store
 	avgResponseTime float         //average response time of write req
 	numReqs         int           //num reqs of interval time
@@ -16,17 +15,17 @@ type StoreState struct {
 type StoreStateList []*StoreState
 
 // cal_score calculating score for all stores
-func (StoreStateList s)calScore() err error {
+func (StoreStateList s) calScore() err error {
 	//
 }
 
 // getWritableStoreGroup get one suitable group store for writing
-func (StoreStateList s)getWritableStoreGroup() []*meta.Store {
+func (StoreStateList s) getWritableStoreGroup() []*meta.Store {
 	//先得到每个store的得分，然后分组，再计算每组的得分  每组的得分为该组的store的最低分
 }
 
 // getReadableStoreGroup get one suitable group store fro reading
-func (StoreStateList s)getReadableStoreGroup() []*meta.Store {
+func (StoreStateList s) getReadableStoreGroup() []*meta.Store {
 	//
 }
 
