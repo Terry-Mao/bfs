a4878ee2c6452088030a5bd6a78e77835674e6a0
zhapuyu <zhapuyu@sina.com>
2015-11-30 16:31:07 +0800

    fix

--- libs/meta/volume.go
+++ libs/meta/volume.go
@@ -3,9 +3,9 @@ package meta
 /*
 // volume zk meta data.
 type Volume struct {
-	NumReqs         string `json:"numReqs"`
-	RestSpace       string `json:"restSpace"`
-	AvgResponseTime string `json:"avgResponseTime"`
+	NumReqs         int    `json:"numReqs"`
+	RestSpace       int    `json:"restSpace"`
+	AvgResponseTime float  `json:"avgResponseTime"`
 	Id              string `json:"id"`
 }
 */
@@ -13,7 +13,7 @@ type Volume struct {
 type Volume struct {
 	Id           int32       `json:"id"`
 	Block        *SuperBlock `json:"block"`
-	CheckNeedles []*Needle   `json:"check_needles"`
+	CheckNeedles []Needle   `json:"check_needles"`
 	//Stats   *stat.Stats       `json:"stats"`
 	//Indexer *index.Indexer    `json:"index"`
 }
