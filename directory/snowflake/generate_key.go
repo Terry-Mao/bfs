02748f3bb054af0059c8e9d9e09affc19c02ac13
zhapuyu <zhapuyu@sina.com>
2015-12-09 19:22:04 +0800

    fix

--- directory/snowflake/generate_key.go
+++ directory/snowflake/generate_key.go
@@ -1,24 +1,24 @@
 package snowflake
 
 import (
+	"errors"
 	log "github.com/golang/glog"
 	"time"
-	"errors"
 )
 
 const (
-	maxSize        = 1000
-	errorSleep     = 1 * time.Second
-	genKeyTimeout  = 2 * time.Second
+	maxSize       = 1000
+	errorSleep    = 1 * time.Second
+	genKeyTimeout = 2 * time.Second
 )
 
 // Genkey generate key for upload file
 type Genkey struct {
-	client    *Client
-	keys      chan int64
+	client *Client
+	keys   chan int64
 }
 
-// NewGenkey 
+// NewGenkey
 func NewGenkey(zservers []string, zpath string, ztimeout time.Duration, workerId int64) (g *Genkey, err error) {
 	if err = Init(zservers, zpath, ztimeout); err != nil {
 		log.Errorf("NewGenkey Init error(%v)", err)
@@ -27,7 +27,7 @@ func NewGenkey(zservers []string, zpath string, ztimeout time.Duration, workerId
 	g = &Genkey{}
 	g.client = NewClient(workerId)
 	g.keys = make(chan int64, maxSize)
-	go g.preGenerate()
+	g.preGenerate()
 	return
 }
 
@@ -45,19 +45,26 @@ func (g *Genkey) Getkey() (key int64, err error) {
 // preGenerate pre generate key until 1000
 func (g *Genkey) preGenerate() {
 	var (
-		key   int64
-		keys  []int64
-		err   error
+		i int
 	)
 	time.Sleep(errorSleep)
-	for {
-		if keys, err = g.client.Ids(100); err != nil {
-			log.Errorf("preGenerate() error(%v)  retry", err)
-			time.Sleep(errorSleep)
-			continue
-		}
-		for _, key = range keys {
-			g.keys <- key
-		}
+	for i = 0; i < 10; i++ {
+		go func() {
+			var (
+				key  int64
+				keys []int64
+				err  error
+			)
+			for {
+				if keys, err = g.client.Ids(100); err != nil {
+					log.Errorf("preGenerate() error(%v)  retry", err)
+					time.Sleep(errorSleep)
+					continue
+				}
+				for _, key = range keys {
+					g.keys <- key
+				}
+			}
+		}()
 	}
 }
--- directory/snowflake/generate_key_test.go
+++ directory/snowflake/generate_key_test.go
@@ -1,29 +1,27 @@
 package snowflake
 
 import (
+	"fmt"
 	"testing"
 	"time"
-	"fmt"
 )
 
-
 func TestSnowflake(t *testing.T) {
-    var (
-            err             error
-            genkey          *Genkey
-            i               int
-            key             int64
-    )
-    if genkey, err = NewGenkey([]string{"localhost:2181"}, "/gosnowflake-servers", time.Second * 15, 0); err != nil {
-        t.Errorf("NewGenkey failed error(%v)",err)
-        t.FailNow()
-    }
-    time.Sleep(3 * time.Second)  // wait rpc
-    for i = 0; i < 10000; i++ {
-        if key, err = genkey.Getkey(); err != nil {
-            t.Errorf("Getkey failed error(%v)", err)
-            t.FailNow()
-        }
-        fmt.Println("key ",i,":", key)
-    }
+	var (
+		err    error
+		genkey *Genkey
+		i      int
+		key    int64
+	)
+	if genkey, err = NewGenkey([]string{"localhost:2181"}, "/gosnowflake-servers", time.Second*15, 0); err != nil {
+		t.Errorf("NewGenkey failed error(%v)", err)
+		t.FailNow()
+	}
+	for i = 0; i < 100000; i++ {
+		if key, err = genkey.Getkey(); err != nil {
+			t.Errorf("Getkey failed error(%v)", err)
+			t.FailNow()
+		}
+		fmt.Println("key ", i, ":", key)
+	}
 }
